9b0380d93db90abbfb828969908ca3f8dca6d264
Sebastian Vera <u.verainf@gmail.com>
2016-07-04 22:27:26 -0400

    Add span tracing logic here
    If this functions handles all the API http responses,
    it'll be convenient to add the tracing output here,
    wether we respond with an error or a success response,
    both responses are properly traced depending on its type.
    
    When an error occurs, we tell the span that there was an error.
    On each response we tell the span the returned http status code.

--- io/response_writer.go
+++ io/response_writer.go
@@ -7,32 +7,44 @@ import (
 
 	"github.com/Finciero/errors"
 	"github.com/Finciero/log"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
 )
 
+// JSONResponseWriter ...
 type JSONResponseWriter struct {
 	http.ResponseWriter
 	Logger log.Logger
+	Span   opentracing.Span
 }
 
 // NewJSONResponseWriter ...
-func NewJSONResponseWriter(w http.ResponseWriter, l log.Logger) *JSONResponseWriter {
+func NewJSONResponseWriter(w http.ResponseWriter, l log.Logger, s opentracing.Span) *JSONResponseWriter {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
 	return &JSONResponseWriter{
 		ResponseWriter: w,
 		Logger:         l,
+		Span:           s,
 	}
 }
 
 // WriteError ...
 func (w *JSONResponseWriter) WriteError(err error) {
 	v := errors.BuildError(err)
+	if v.StatusCode == errors.StatusInternalServerError {
+		w.Logger.Error(v)
+	}
+
+	ext.Error.Set(w.Span, true)
+
 	w.Write(v.Code(), v)
 }
 
 // Write ...
 func (w *JSONResponseWriter) Write(code int, v interface{}) {
 	w.WriteHeader(code)
+	ext.HTTPStatusCode.Set(w.Span, uint16(code))
 
 	err := json.NewEncoder(w.ResponseWriter).Encode(v)
 
